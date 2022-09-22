2e697568b4baa227fbabda88543e06b36697e73d
harutiro <harutiro2027@outlook.jp>
2022-09-22 09:48:36 +0900

    fix: helloworldの表記の仕方のミスを訂正

--- prg1/prog02.c
+++ prg1/prog02.c
@@ -4,7 +4,7 @@ int main(int argc, const char* argv[]){
 
 
     for(int i = 0 ; i < 10 ; i++){
-        printf("Hello World!\n");
+        printf("Hello, world!\n");
     }
 
     return 0;
--- prg1/prog03.c
+++ prg1/prog03.c
@@ -3,12 +3,12 @@
 int main(int argc, const char* argv[]){
 
     int input = 0;
-    
+
     printf("Enter a number: ");
     scanf("%d", &input);
 
     for(int i = 0 ; i < input ; i++){
-        printf("Hello World!\n");
+        printf("Hello, world!\n");
     }
 
     return 0;
