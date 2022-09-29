bb1f53025919b32e48f6f618bc25c73180d09512
harutiro <harutiro2027@outlook.jp>
2022-09-29 10:57:44 +0900

    fact: (B-4)：次の関数を作れ

--- prg2/04_bit/bin.c
+++ prg2/04_bit/bin.c
@@ -14,12 +14,49 @@ int printbin(unsigned int x){
     return 0;
 }
 
+unsigned int rrotate(unsigned int x, int n){
+    unsigned int mask = 0;
+    for(int i = 0 ; i < n ; i++){
+        mask = mask | (1 << i);
+    }
+
+    unsigned int tmp = x & mask;
+    tmp = tmp << (32 - n);
+    x = x >> n;
+    x = x | tmp;
+
+    return x;
+}
+
+unsigned int lrotate(unsigned int x, int n){
+    unsigned int mask = 0;
+    for(int i = 0 ; i < n ; i++){
+        mask = mask | (1 << (31 - i));
+    }
+
+    unsigned int tmp = x & mask;
+    tmp = tmp >> (32 - n);
+    x = x << n;
+    x = x | tmp;
+
+    return x;
+}
+
 int main(int argc, const char* argv[]){
     int num = 0;
     printf("input number: ");
     scanf("%d", &num);
 
+    int roll = 0;
+    printf("input roll: ");
+    scanf("%d", &roll);
+
+    printf("num    : ");
     printbin(num);
+    printf("rrotate: ");
+    printbin(rrotate(num, roll));
+    printf("lrotate: ");
+    printbin(lrotate(num, roll));
 
     return 0;
 }
