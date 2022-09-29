eb09e5c6dc797dd928c0546705065279a140c91e
harutiro <harutiro2027@outlook.jp>
2022-09-29 09:48:51 +0900

    fact: A-6 完成

--- prg2/03_enshuFig/enshuFig-sub5.c
+++ prg2/03_enshuFig/enshuFig-sub5.c
@@ -4,7 +4,7 @@ int draw_figure(int n){
 
     for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < n ; j++){
-            if(i >= n-1-j){
+            if( i + j == 4){
                 printf("*");
             }else{
                 printf(" ");
