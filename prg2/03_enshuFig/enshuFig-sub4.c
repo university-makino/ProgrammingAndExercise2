78b9b79a07445eb6f8893fb85081013a13c796b0
harutiro <harutiro2027@outlook.jp>
2022-09-29 09:47:05 +0900

    fact: 条件のTrueFalseの流れを逆にした

--- prg2/03_enshuFig/enshuFig-sub4.c
+++ prg2/03_enshuFig/enshuFig-sub4.c
@@ -4,10 +4,10 @@ int draw_figure(int n){
 
     for(int i = 0 ; i < n ; i++){
         for(int j = 0 ; j < n ; j++){
-            if(i < n-1-j){
-                printf(" ");
-            }else{
+            if(i >= n-1-j){
                 printf("*");
+            }else{
+                printf(" ");
             }
         }
         printf("\n");
