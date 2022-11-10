4f1681be4df7e64b96b41890b31e4d4bf3885b83
harutiro <harutiro2027@outlook.jp>
2022-11-10 09:19:47 +0900

    update: 読み込みできるようになった。

---
+++ prg6/prg5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 256
+
+int main(void) {
+    char line[MAX];  // １⾏分を読み込むための配列
+
+    FILE *fp;
+    fp = fopen("meibo.txt", "r");
+    if (fp == NULL) {
+        printf("Cannot open the file.");
+        exit(0);
+    }
+
+    // ファイル処理をここに書く
+    while (fgets(line, MAX, fp) != NULL) {
+        printf("%s¥n", line);
+    }
+
+    fclose(fp);
+    return 0;
+}
