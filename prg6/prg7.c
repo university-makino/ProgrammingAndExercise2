46a83c6c1246ace83d1277a0f3e718f30d988050
harutiro <harutiro2027@outlook.jp>
2022-11-10 09:31:58 +0900

    update: ファイルに追記するプログラムを作成した。

---
+++ prg6/prg7.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 256
+
+int main(void) {
+    char line[MAX];  // １⾏分を読み込むための配列
+
+    FILE *fp;
+    fp = fopen("number.txt", "a");
+    if (fp == NULL) {
+        printf("Cannot open the file.");
+        exit(0);
+    }
+
+    // ファイル処理をここに書く
+    for(int i = 100; i < 200; i++) {
+        char str[10] = {'\0'};
+        sprintf(str, "%d\n", i);
+        fputs(str, fp);
+    }
+
+    fclose(fp);
+    return 0;
+}
