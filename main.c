97ea7c0e2e69778684a88200d96157a3bff47dcc
harutiro <harutiro2027@outlook.jp>
2022-11-17 09:33:32 +0900

    update:説明の追加。

--- main.c
+++ main.c
@@ -1,30 +1,12 @@
-//
-//  main.c
-//  prog5
-//
-//  Created by k18065kk on 2018/11/15.
-//  Copyright © 2018年 k18065kk. All rights reserved.
-//
-
 #include <stdio.h>
-#include <stdlib.h>
-#define MAX 256
 
-int main(int argc, const char * argv[]) {
+int main(int argc, const char* argv[]){
 
-    FILE *fp;
-    fp=fopen("meibo.txt","r");
-    if(fp==NULL){
-        printf("Cannot open the file.\n");
-        exit(0);
-    }
+    char *str;
 
-    char line[MAX];
+    str = "testdayo";
 
-    while(fgets(line, MAX, fp) != NULL){
-        printf("%s\n",line);
-    }
-    
-    fclose(fp);
+    printf("%s\n",str);
+
     return 0;
 }
--- prg7/prog2.c
+++ prg7/prog2.c
@@ -6,6 +6,8 @@ int main(int argc, const char* argv[]) {
 
     char *pointer = (char*)malloc(5 * sizeof(char));
 
+    // pointer = "hoge" これはダメ。
+    // "hoge"をしたタイミングで、hogeをどこか知らないアドレスに展開されて、その先頭のアドレスを突っ込まれるため、freeができなくなる。
     strcpy(pointer, "hoge");
 
     printf("pointer = %s\n", pointer);
