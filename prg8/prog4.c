162bc34e9c7667ba36c2a85b0a1f2c162fd01221
harutiro <harutiro2027@outlook.jp>
2022-11-24 10:15:42 +0900

    update:変数名の変更

--- prg8/prog4.c
+++ prg8/prog4.c
@@ -18,30 +18,30 @@ void printIris(Iris iris){
 int main(int argc, const char* argv[]){
 
     char line[50]="5.7,2.9,4.2,1.3,Iris-versicolor\n";
-    char *token;
+    char *pointer;
     Iris iris;
     
     //,を用いて文字列を分割
     //https://secret-garden.hatenablog.com/entry/2015/09/02/000000
-    token = strtok(line, ",");
-    
+    pointer = strtok(line, ",");
+
     // atofを用いて、文字列をdouble型に変換
     //https://programming-place.net/ppp/contents/c/rev_res/string004.html
 
-    iris.sepalLength = atof(token);
-    token = strtok(NULL, ",");
-    iris.sepalWidth = atof(token);
-    token = strtok(NULL, ",");
-    iris.petalLength = atof(token);
-    token = strtok(NULL, ",");
-    iris.petalWidth = atof(token);
-    token = strtok(NULL, ",");
+    iris.sepalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.sepalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
 
     //改行を対策する。
     // 改行文字は最後にあることを決め打ちしている。
     //https://okwave.jp/qa/q4452927.html
-    token[strlen(token)-1]='\0';
-    iris.species = token;
+    pointer[strlen(pointer)-1]='\0';
+    iris.species = pointer;
 
     
 
