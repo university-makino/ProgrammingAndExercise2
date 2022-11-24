614fb951d7ad377fe00ae627a56d290aca8233f7
harutiro <harutiro2027@outlook.jp>
2022-11-24 11:07:41 +0900

    update:配列に変更することで全てが解決した。

--- prg8/prog5.c
+++ prg8/prog5.c
@@ -10,7 +10,7 @@ typedef struct iris {
     double sepalWidth;
     double petalLength;
     double petalWidth;
-    char *species;
+    char species[32];
 }Iris;
 
 void printIris(Iris iris){
@@ -18,9 +18,39 @@ void printIris(Iris iris){
     return ;
 }
 
+Iris irisSprit(char *str){
+    char *pointer;
+    Iris iris;
+    
+    //,を用いて文字列を分割
+    //https://secret-garden.hatenablog.com/entry/2015/09/02/000000
+    pointer = strtok(str, ",");
+
+    // atofを用いて、文字列をdouble型に変換
+    //https://programming-place.net/ppp/contents/c/rev_res/string004.html
+
+    iris.sepalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.sepalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+
+    //改行を対策する。
+    // 改行文字は最後にあることを決め打ちしている。
+    //https://okwave.jp/qa/q4452927.html
+    pointer[strlen(pointer)-1]='\0';
+    static char tmp[64] = {'\0'};
+    strcpy(iris.species, pointer);
+
+    return iris;
+}
+
 int main(int argc, const char* argv[]){
 
-    Iris *lists;
+    Iris lists[256];
     char line[MAX];  // １⾏分を読み込むための配列
 
     FILE *fp;
@@ -31,8 +61,16 @@ int main(int argc, const char* argv[]){
     }
 
     // ファイル処理をここに書く
+    int i=0;
     while (fgets(line, MAX, fp) != NULL) {
-        printf("%s", line);
+        lists[i] = irisSprit(line);
+        i++;
+    }
+    // printf("%d\n",i);
+
+    for(int j=0;j<i;j++){
+        // printf("%d",j);
+        printIris(lists[j]);
     }
 
     fclose(fp);
