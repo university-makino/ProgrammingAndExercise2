eb40fb38dcb48cf8b8c7f41152ee3239bbcf70ef
harutiro <harutiro2027@outlook.jp>
2022-11-24 15:33:45 +0900

    update:しっかりした名前を入れた

--- prg8/prog1.c
+++ prg8/prog1.c
@@ -13,10 +13,10 @@ void printStudent(struct student s){
 }
 
 int main(int argc, const char* argv[]){
-    struct student s1 = {"test", 20, "123456", 3};
-    struct student s2 = {"test2", 21, "1234567", 4};
-    struct student s3 = {"test3", 22, "12345678", 5};
-    struct student s4 = {"test4", 23, "123456789", 6};
+    struct student s1 = {"makino", 19, "k22130", 1};
+    struct student s2 = {"owashiro", 20, "x21011", 2};
+    struct student s3 = {"ogane", 21, "l20123", 3};
+    struct student s4 = {"kawashima", 22, "b13123", 4};
 
     printStudent(s1);
     printStudent(s2);
