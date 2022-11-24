4036874d9b6de8f456359c695f4742ec36f1c707
harutiro <harutiro2027@outlook.jp>
2022-11-24 11:25:04 +0900

    update:平均を表示させた。

---
+++ prg8/prog6.c
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX 256
+
+
+typedef struct iris {
+    double sepalLength;
+    double sepalWidth;
+    double petalLength;
+    double petalWidth;
+    char species[32];
+}Iris;
+
+void printIris(Iris iris){
+    printf("sepalLength: %f, sepalWidth: %f, petalLength: %f, petalWidth: %f, species: %s\n", iris.sepalLength, iris.sepalWidth, iris.petalLength, iris.petalWidth, iris.species);
+    return ;
+}
+
+Iris irisSprit(char *str){
+    char *pointer;
+    Iris iris;
+    
+    //,を用いて文字列を分割
+    //https://secret-garden.hatenablog.com/entry/2015/09/02/000000
+    pointer = strtok(str, ",");
+
+    // atofを用いて、文字列をdouble型に変換
+    //https://programming-place.net/ppp/contents/c/rev_res/string004.html
+
+    iris.sepalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.sepalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+
+    //改行を対策する。
+    // 改行文字は最後にあることを決め打ちしている。
+    //https://okwave.jp/qa/q4452927.html
+    pointer[strlen(pointer)-1]='\0';
+    static char tmp[64] = {'\0'};
+    strcpy(iris.species, pointer);
+
+    return iris;
+}
+
+void printIrisAverage(Iris *iris_list, int size){
+    double sepalLength_Average = 0;
+    double sepalWidth_Average = 0;
+    double petalLength_Average = 0;
+    double petalWidth_Average = 0;
+
+    for(int j=0;j<size;j++){
+        sepalLength_Average += iris_list[j].sepalLength;
+        sepalWidth_Average += iris_list[j].sepalWidth;
+        petalLength_Average += iris_list[j].petalLength;
+        petalWidth_Average += iris_list[j].petalWidth;
+        // printIris(iris_list[j]);
+    }
+
+    sepalLength_Average = sepalLength_Average/size;
+    sepalWidth_Average = sepalWidth_Average/size;
+    petalLength_Average = petalLength_Average/size;
+    petalWidth_Average = petalWidth_Average/size;
+
+    printf("virginica: sepalLength: %f, sepalWidth: %f, petalLength: %f, petalWidth: %f\n", sepalLength_Average, sepalWidth_Average, petalLength_Average, petalWidth_Average);
+
+}
+
+int main(int argc, const char* argv[]){
+
+    Iris lists[MAX];
+    char line[MAX];  // １⾏分を読み込むための配列
+    Iris iris_virginica_list[MAX];
+    Iris iris_versicolor_list[MAX];
+    Iris iris_setosa_list[MAX];
+
+    FILE *fp;
+    fp = fopen("iris.data", "r");
+    if (fp == NULL) {
+        printf("Cannot open the file.");
+        exit(0);
+    }
+
+    // ファイル処理をここに書く
+    int i=0;
+    while (fgets(line, MAX, fp) != NULL) {
+        lists[i] = irisSprit(line);
+        i++;
+    }
+
+    // リストを仕分ける。
+    int iris_virginica_count = 0;
+    int iris_versicolor_count = 0;
+    int iris_setosa_count = 0;
+
+    for(int j=0;j<i;j++){
+        if(strcmp(lists[j].species,"Iris-virginica")==0){
+            iris_virginica_list[iris_virginica_count] = lists[j];
+            iris_virginica_count++;
+
+        }else if(strcmp(lists[j].species,"Iris-versicolor")==0){
+            iris_versicolor_list[iris_versicolor_count] = lists[j];
+            iris_versicolor_count++;
+
+        }else if(strcmp(lists[j].species,"Iris-setosa")==0){
+            iris_setosa_list[iris_setosa_count] = lists[j];
+            iris_setosa_count++;
+
+        }
+    }
+
+    // リストで平均を出す。
+    printIrisAverage(iris_virginica_list, iris_virginica_count);
+    printIrisAverage(iris_versicolor_list, iris_versicolor_count);
+    printIrisAverage(iris_setosa_list, iris_setosa_count);
+
+
+    fclose(fp);
+
+    // printIris(iris);
+
+    return 0;
+}
