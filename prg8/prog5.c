3b6660bc9f683f5687a3cc5c7761b23935efa418
harutiro <harutiro2027@outlook.jp>
2022-11-24 10:25:25 +0900

    update:ファイルを読み込む部分完成

--- prg8/prog5.c
+++ prg8/prog5.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define MAX 256
+
+
 typedef struct iris {
     double sepalLength;
     double sepalWidth;
@@ -18,10 +21,23 @@ void printIris(Iris iris){
 int main(int argc, const char* argv[]){
 
     Iris *lists;
+    char line[MAX];  // １⾏分を読み込むための配列
 
-    
+    FILE *fp;
+    fp = fopen("iris.data", "r");
+    if (fp == NULL) {
+        printf("Cannot open the file.");
+        exit(0);
+    }
 
-    printIris(iris);
+    // ファイル処理をここに書く
+    while (fgets(line, MAX, fp) != NULL) {
+        printf("%s", line);
+    }
+
+    fclose(fp);
+
+    // printIris(iris);
 
     return 0;
 }
