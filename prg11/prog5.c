1643e5d877e6da87ad6bfa81a56de3a46e01f06d
harutiro <harutiro2027@outlook.jp>
2022-12-15 09:20:40 +0900

    update: ランダムに表示するところまで完成。

---
+++ prg11/prog5.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define MAX 256
+
+
+typedef struct iris {
+    double sepalLength;
+    double sepalWidth;
+    double petalLength;
+    double petalWidth;
+    char species[32];
+}Iris;
+
+void printIris(Iris iris){
+    printf("sepalLength: %f, sepalWidth: %f, petalLength: %f, petalWidth: %f, species: %s\n", iris.sepalLength, iris.sepalWidth, iris.petalLength, iris.petalWidth, iris.species);
+    return ;
+}
+
+Iris irisSprit(char *str){
+    char *pointer;
+    Iris iris;
+    
+    //,を用いて文字列を分割
+    //https://secret-garden.hatenablog.com/entry/2015/09/02/000000
+    pointer = strtok(str, ",");
+
+    // atofを用いて、文字列をdouble型に変換
+    //https://programming-place.net/ppp/contents/c/rev_res/string004.html
+
+    iris.sepalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.sepalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalLength = atof(pointer);
+    pointer = strtok(NULL, ",");
+    iris.petalWidth = atof(pointer);
+    pointer = strtok(NULL, ",");
+
+    //改行を対策する。
+    // 改行文字は最後にあることを決め打ちしている。
+    //https://okwave.jp/qa/q4452927.html
+    pointer[strlen(pointer)-1]='\0';
+    static char tmp[64] = {'\0'};
+    strcpy(iris.species, pointer);
+
+    return iris;
+}
+
+void randomize(Iris a[], int index){
+    srand((unsigned int)time(NULL));
+
+    for(int i = index - 1; i > 0; i--){
+        int j = rand() % i;
+
+        Iris temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
+
+int main(int argc, const char* argv[]){
+
+    Iris lists[MAX];
+    char line[MAX];  // １⾏分を読み込むための配列
+    Iris iris_virginica_list[MAX];
+    Iris iris_versicolor_list[MAX];
+    Iris iris_setosa_list[MAX];
+
+    FILE *fp;
+    fp = fopen("iris.data", "r");
+    if (fp == NULL) {
+        printf("Cannot open the file.");
+        exit(0);
+    }
+
+    // ファイル処理をここに書く
+    int i=0;
+    while (fgets(line, MAX, fp) != NULL) {
+        lists[i] = irisSprit(line);
+        i++;
+    }
+    // 配列のサイズを取得
+    int size = i;
+
+    randomize(lists,size);
+
+    for(int i = 0; i < size; i++){
+        printIris(lists[i]);
+    }
+
+    fclose(fp);
+
+    return 0;
+}
