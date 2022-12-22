a0aa27698eb4f6dcb10b7aa6d76c614ec2930932
harutiro <harutiro2027@outlook.jp>
2022-12-22 11:33:22 +0900

    update: ソートの仕方を変更した。

--- prg12/prog5.c
+++ prg12/prog5.c
@@ -11,7 +11,6 @@ typedef struct elem{
 
 void printList(Elem *root);
 Elem* addElem(Elem *root, int num);
-Elem* addElemFirst(Elem *root, int num);
 void freeList(Elem *root);
 void sortElem(Elem *root);
 
@@ -40,28 +39,60 @@ void printList(Elem *root){
     }
 }
 
+
 Elem* addElem(Elem *root, int num){
-    Elem *p = root;
-    while(p->next != NULL){
-        p = p->next;
+
+    Elem *current = root;
+    Elem *prev = NULL;
+
+    while(current != NULL){
+        if(current->num > num){
+            break;
+        }
+        prev = current;
+        current = current->next;
     }
-    Elem *newElem = (Elem *)malloc(sizeof(Elem));
+
+    Elem *newElem = (Elem*)malloc(sizeof(Elem));
     newElem->num = num;
-    newElem->next = NULL;
-    p->next = newElem;
 
-    sortElem(root);
+    //  場合分けを行う。
+    /*
+    if(current == NULL && prev == NULL){
+        //空っぽだった場合は先頭に追加
+        newElem->next = NULL;
+        return newElem;
 
-    return root;
+    }else if(current == NULL && prev != NULL){
+        //一個も見つからば買った場合は末尾に追加
+        newElem->next = NULL;
+        prev->next = newElem;
+        return root;
 
-}
+    }else if(prev == NULL){
+        //先頭に要素を挿入する場合。（ROOTの値が入れたい値より大きい場合）
+        newElem->next = root;
+        return newElem;
 
-Elem* addElemFirst(Elem *root, int num){
-    Elem *newElem = (Elem *)malloc(sizeof(Elem));
-    newElem->num = num;
-    newElem->next = root;
-    root = newElem;
-    return root;
+    }else{
+        //prevとcurrentの間に追加
+        newElem->next = current;
+        prev->next = newElem;
+        return root;
+    }
+    */
+
+    // 場合わけをきれいにする。
+    if(prev == NULL){
+        //空っぽだった場合は先頭に追加
+        newElem->next = NULL;
+        return newElem;
+    }else{
+        //prevとcurrentの間に追加
+        newElem->next = current;
+        prev->next = newElem;
+        return root;
+    }
 }
 
 void freeList(Elem *root){
@@ -78,13 +109,11 @@ int main(int argc, const char* argv[]){
     Elem *root = NULL;
 
     // 先頭に追加
-    root = addElemFirst(root, 10);
-
-    // 末尾に追加
-    addElem(root, 40);
-    addElem(root, 30);
-    addElem(root, 20);
-    addElem(root, 50);
+    root = addElem(root, 10);
+    root = addElem(root, 40);
+    root = addElem(root, 30);
+    root = addElem(root, 20);
+    root = addElem(root, 50);
 
     // 出力
     printList(root);
