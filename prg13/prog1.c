188307107253cf74e2ea27b80b3c4ba22132a2cb
harutiro <harutiro2027@outlook.jp>
2023-01-12 09:16:42 +0900

    update: エラーを解決。

--- prg13/prog1.c
+++ prg13/prog1.c
@@ -4,8 +4,8 @@ typedef struct melem{
     char name[256];
     int height;
     int weight;
-    struct melem *next
-}Melem
+    struct melem *next;
+}Melem;
 
 int main(int argc, const char* argv[]){
 
