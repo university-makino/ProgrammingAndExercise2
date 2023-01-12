b169ecd25abe4f62cdb1cbbcd83a1d66b159673c
harutiro <harutiro2027@outlook.jp>
2023-01-12 09:15:53 +0900

    update: 型を定義した。

---
+++ prg13/prog1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+typedef struct melem{
+    char name[256];
+    int height;
+    int weight;
+    struct melem *next
+}Melem
+
+int main(int argc, const char* argv[]){
+
+
+
+    return 0;
+}
