4fec879937c2e7efb5aebb9eac5085a88abbc569
harutiro <harutiro2027@outlook.jp>
2023-01-19 09:43:59 +0900

    update: 変換する。

--- prg14/juroku2.c
+++ prg14/juroku2.c
@@ -19,14 +19,18 @@ int main(int argc, const char* argv[]){
     int r = 0;
     int q = number;
 
+    //16進数に変換するにあたり、各値を配列に格納する
     int i = 0;
     do{
+        //16進数の角位の値を求める
         r = q % 16;
         q = q / 16;
+        //16進数の角位の値を配列に格納する
         hex[i] = showHex(r);
         i++;
     }while(q != 0);
 
+    //出力部分
     for(int j = i-1; j != -1; j--){
         putchar(hex[j]);
     }
